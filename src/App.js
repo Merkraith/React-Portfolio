5614d34bf5b5009093a248e87de49e38abe93ca8
Mark Heil <mark.heil1122@gmail.com>
2020-08-24 00:21:35 -0700

    updated app.js with AppBar component, added card components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import AppBar from './components/AppBar'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <AppBar/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
