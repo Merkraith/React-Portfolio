ac589d3b52dfdc120d3fe2cd61fbdc0481c13d1d
Mark Heil <mark.heil1122@gmail.com>
2020-09-29 10:37:53 -0700

    revamped Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+import '../../App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+function TopNavbar() {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand>
+                <h4 className="my-name">Mark Heil</h4>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink to={"/"} className="link" exact activeClassName="active"><p className="nav-link pt-0 pb-0" id="aboutLink" data-toggle="collapse" data-target=".navbar-collapse.show">Home</p></NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={"../../Pages/About"} className="link" activeClassName="active"><p className="nav-link pt-0 pb-0" id="aboutLink" data-toggle="collapse" data-target=".navbar-collapse.show">About</p></NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={"../../Pages/GroupProjects"} className="link" activeClassName="active"><p className="nav-link pt-0 pb-0" id="groupProjectsLink" data-toggle="collapse" data-target=".navbar-collapse.show">Group Projects</p></NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={"../../Pages/Projects"} className="link" activeClassName="active"><p className="nav-link pt-0 pb-0 lastLink" id="projectsLink" data-toggle="collapse" data-target=".navbar-collapse.show">Projects</p></NavLink>
+                            </li>
+                        </ul>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+}
+
+export default TopNavbar;
